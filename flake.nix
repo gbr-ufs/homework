125edf25160f730a7a53c16a93d0bd4665418590
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-07-22 15:49:45 -0300

    feat(flake.nix): add flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# Este flake é impuro porque ele usa a função "currentSystem".
+# Isso é melhor do que ter que depender no flake-utils, e funciona como uma
+# forma de "protesto" contra a verbosidade dos flakes.
+
+{
+  description = "Exercícios resolvidos.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+  system = builtins.currentSystem; # Impureza aqui.
+  pkgs = nixpkgs.legacyPackages.${system};
+
+  in {
+    devShell.${system} = pkgs.mkShell {
+      packages = with pkgs; [
+        nodejs
+        vtsls
+      ];
+    };
+  };
+}
