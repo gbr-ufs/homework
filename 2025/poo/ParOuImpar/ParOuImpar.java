d1baf374e0f350d9e78375f263acbcbf1f885204
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-10-28 14:12:52 -0300

    refactor(ParOuImpar.java): use io object
    Use the IO object that was stabilized in JDK 25 instead of Console,
    for ease of use.

--- 2025/poo/ParOuImpar/ParOuImpar.java
+++ 2025/poo/ParOuImpar/ParOuImpar.java
@@ -1,11 +1,8 @@
-import java.io.Console;
-
 public class ParOuImpar {
     public static void main(String[] args) {
         int numero;
-        Console console = System.console();
 
-        numero = Integer.parseInt(console.readLine("Digite um número: "));
+        numero = Integer.parseInt(IO.readln("Digite um número: "));
 
         if (numero % 2 == 0) {
             System.out.println("É par");
