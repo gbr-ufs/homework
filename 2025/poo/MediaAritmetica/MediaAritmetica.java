9a2f81d2fd86a140ed8e319574879d2e0a80ea84
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-10-28 14:14:51 -0300

    feat(MediaAritmetica.java): add method as intended
    Implement a mediaAritmetica as is actually solicited by the question.
    
    refactor: use io object

--- 2025/poo/MediaAritmetica/MediaAritmetica.java
+++ 2025/poo/MediaAritmetica/MediaAritmetica.java
@@ -1,17 +1,28 @@
-import java.io.Console;
+class Media {
+
+    public double mediaAritmetica(double[] valores) {
+        double soma = 0;
+
+        for (int i = 0; i < valores.length; i++) {
+            soma += valores[i];
+        }
+
+        return soma / valores.length;
+    }
+}
 
 public class MediaAritmetica {
     public static void main(String[] args) {
-        double primeiro, segundo, terceiro, media;
+        double primeiro, segundo, terceiro;
         String mensagem = "A média aritmética destes três números é: ";
-        Console console = System.console();
+        Media media = new Media();
 
-        primeiro = Double.parseDouble(console.readLine("Digite o primeiro número: "));
-        segundo = Double.parseDouble(console.readLine("Digite o segundo número: "));
-        terceiro = Double.parseDouble(console.readLine("Digite o terceiro número: "));
+        primeiro = Double.parseDouble(IO.readln("Digite o primeiro número: "));
+        segundo = Double.parseDouble(IO.readln("Digite o segundo número: "));
+        terceiro = Double.parseDouble(IO.readln("Digite o terceiro número: "));
 
-        media = (primeiro + segundo + terceiro) / 3;
-        mensagem += media;
+        double[] valores = {primeiro, segundo, terceiro};
+        mensagem += media.mediaAritmetica(valores);
         System.out.println(mensagem);
     }
 }
