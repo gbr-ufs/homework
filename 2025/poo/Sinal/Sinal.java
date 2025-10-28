9e37451111ee1e8bb95044ad70e30bd4d0ac5af7
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-10-28 10:32:53 -0300

    refactor(Sinal.java): use io object
    Use the IO object that was stabilized in JDK 25 instead of Console,
    for ease of use.

--- 2025/poo/Sinal/Sinal.java
+++ 2025/poo/Sinal/Sinal.java
@@ -1,11 +1,8 @@
-import java.io.Console;
-
 public class Sinal {
     public static void main(String[] args) {
         int numero;
-        Console console = System.console();
 
-        numero = Integer.parseInt(console.readLine("Digite um número: "));
+        numero = Integer.parseInt(IO.readln("Digite um número: "));
 
         if (numero == 0) {
             System.out.println("Nulo");
