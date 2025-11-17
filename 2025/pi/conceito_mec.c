15dc60c9460ef820227934ae751880d2e2030cca
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2025-11-17 17:52:22 -0300

    refactor(conceito_mec.c): do alunos/livros instead of livros/alunos

--- 2025/pi/conceito_mec.c
+++ 2025/pi/conceito_mec.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 
-int intervalo(double inicio, double valor, double fim) {
-  if (valor <= inicio && valor >= fim) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
 int main() {
   // Entrada.
   long quantidade_de_livros, quantidade_de_alunos;
   // Saída.
   double escala_livro_aluno;
-  double conceito_a = 1.0 / 8.0;
-  double conceito_b_inicio = 1.0 / 9.0, conceito_b_fim = 1.0 / 12.0;
-  double conceito_c_inicio = 1.0 / 13.0, conceito_c_fim = 1.0 / 18.0;
+  long conceito_a = 8;
+  long conceito_b = 12;
+  long conceito_c = 18;
 
-  scanf("%ld %ld", &quantidade_de_livros, &quantidade_de_alunos);
+  scanf("%ld %ld", &quantidade_de_alunos, &quantidade_de_livros);
   escala_livro_aluno =
-      (double)quantidade_de_livros / (double)quantidade_de_alunos;
+      (double)quantidade_de_alunos / (double)quantidade_de_livros;
 
-  if (escala_livro_aluno >= conceito_a) {
+  if (escala_livro_aluno <= conceito_a) {
     printf("A\n");
-  } else if (intervalo(conceito_b_inicio, escala_livro_aluno, conceito_b_fim)) {
+  } else if (escala_livro_aluno <= conceito_b) {
     printf("B\n");
-  } else if (intervalo(conceito_c_inicio, escala_livro_aluno, conceito_c_fim)) {
+  } else if (escala_livro_aluno <= conceito_c) {
     printf("C\n");
   } else {
     printf("D\n");
