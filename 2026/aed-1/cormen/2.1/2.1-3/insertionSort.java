bd1b63ed218631885b3d602434e4488402a9d04e
Gabriel Santos de Souza <gabriel.santosdesouza@dcomp.ufs.br>
2026-04-06 21:15:14 -0300

    feat(cormen/2.1/2.1-3): finalizar atividade

---
+++ 2026/aed-1/cormen/2.1/2.1-3/insertionSort.java
@@ -0,0 +1,31 @@
+public class insertionSort {
+    public static void sort(int[] a, int n) {
+        for (int i = 1; i < n; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] < key) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = key;
+        }
+    }
+    public static void main(String[] args) {
+        int a[] = {4, 3, 1, 6, 10};
+        int n = a.length;
+
+        IO.println("Unsorted.");
+
+        for (int i = 0; i < n; i++) {
+            IO.println(a[i]);
+        }
+
+        sort(a, n);
+
+        IO.println("Sorted.");
+
+        for (int i = 0; i < n; i++) {
+            IO.println(a[i]);
+        }
+    }
+}
---
+++ 2026/aed-1/cormen/2.1/2.1-3/insertion_sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void insertion_sort(int *a, int n) {
+  for (int i = 1; i < n; i++) {
+    int key = a[i];
+    int j = i - 1;
+    while (j >= 0 && a[j] < key) {
+      a[j + 1] = a[j];
+      j = j - 1;
+    }
+    a[j + 1] = key;
+  }
+}
+
+int main() {
+  int a[5] = {4, 3, 1, 6, 10};
+  int n = sizeof(a) / sizeof(int);
+
+  std::cout << "Unsorted." << '\n';
+
+  for (int i = 0; i < n; i++) {
+    std::cout << a[i] << '\n';
+  }
+
+  insertion_sort(a, n);
+
+  std::cout << "Sorted." << '\n';
+
+  for (int i = 0; i < n; i++) {
+    std::cout << a[i] << '\n';
+  }
+
+  return 0;
+}
